dc6e96379d75af87c8235df8683751be40075922
Benjamin <47288387+nimajneben@users.noreply.github.com>
2024-05-10 01:52:56 -0400

    Update importer_ingredients_view.dart
    now fetches data from firestore

--- lib/screens/importer_ingredients_view.dart
+++ lib/screens/importer_ingredients_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:convert';
 
+//Importer Ingredients Main Screen Widget
 class ImporterIngredientsView extends StatelessWidget {
   const ImporterIngredientsView({super.key});
 
@@ -8,20 +10,13 @@ class ImporterIngredientsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Available Ingredients')),
-      // body: ListView.builder(
-      //   padding: const EdgeInsets.all(8),
-      //   itemBuilder: (_, index) =>
-      //     Container(
-      //       height: 50,
-      //       color: Colors.amber[600],
-      //       child: const Center(child: Text('test item')),
-      //     )
-      // )
       body: const ImporterIngredientsList(),
     );
   }
 }
 
+// Importer Ingredients Stateful Helper
+// Return the Ingredients List
 class ImporterIngredientsList extends StatefulWidget {
   const ImporterIngredientsList({super.key});
 
@@ -29,25 +24,84 @@ class ImporterIngredientsList extends StatefulWidget {
   State<ImporterIngredientsList> createState() => _ImporterIngredientsList();
 }
 
+// Ingredients List Builder
 class _ImporterIngredientsList extends State<ImporterIngredientsList> {
   final db = FirebaseFirestore.instance;
-  late var docRef;
 
-  void _getData() {
-    docRef = db.collection("Ingredients");
-    const source = Source.server;
-
-    docRef.get(const GetOptions(source: source)).then(
-          (res) => print("Successfully completed"),
-      onError: (e) => print("Error completing: $e"),
+  // Get Ingredients Collection from Firebase
+  Future<List<Ingredient>> _getData() async {
+    List<Ingredient> ingredientList = [];
+    db.collection("Ingredients").get().then(
+      (querySnapshot) {
+        // DEBUG: success message
+        // print("Successfully completed");
+        for (var docSnapshot in querySnapshot.docs) {
+          // DEBUG: print data
+          // print('${docSnapshot.id} => ${docSnapshot.data()}');
+          // Extract data from query
+          final String ingredientName = docSnapshot.id;
+          final Map<String, dynamic> idmap = docSnapshot.data();
+          // Serialization to our Ingredient Object
+          ingredientList.add(
+            Ingredient(
+                name: ingredientName,
+                amount: int.parse(idmap['amount'].toString() ?? '0'),
+                restock: bool.parse(idmap['needRestock'].toString() ?? 'false',)
+            )
+          );
+        }
+      },
+      // DEBUG: For error
+      // onError: (e) => print("Error completing: $e"),
     );
+    return ingredientList;
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => _getData()
+    Future<List<Ingredient>> ingredientList = _getData();
+    // return GestureDetector(
+    //   onTap: () => _getData()
+    // );
+    return FutureBuilder(
+        // FutureBuilder
+        // Show spinner while contents load
+        future: ingredientList,
+        builder: (context,snapshot) {
+          if(snapshot.hasData) {
+            return ListView.builder(
+              // parameter may be null, assert the possibility and provide base case.
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index){
+                return ListTile(
+                  title: Text(snapshot.data?[index].toString() ?? '')
+                );
+              }
+            );}
+          else if (snapshot.hasError) {
+            throw Exception("data error");
+          }
+          return const CircularProgressIndicator();
+      }
     );
   }
 
+}
+
+// In-App Ingredient Object Representation
+class Ingredient {
+  final String name;
+  final int amount;
+  final bool restock;
+
+  Ingredient({
+    required this.name,
+    required this.amount,
+    required this.restock,
+  });
+
+  @override
+  String toString() {
+    return name;
+  }
 }
