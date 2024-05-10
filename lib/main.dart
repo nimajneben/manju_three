996a4039fd7329ba1035d42d68375ee8ff088cb9
Benjamin <47288387+nimajneben@users.noreply.github.com>
2024-05-10 01:52:20 -0400

    add firebase and local storage libraries to requirements

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import './screens/importer_menu.dart';
 
-void main() {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
